989d4897a834b5defb3b478589716e33fa54d4ae
NoobMaster98 <abhishek.bhushan10@gmail.com>
2020-06-01 05:27:33 +0530

    Update Sounds File

--- SoundEffects.py
+++ SoundEffects.py
@@ -3,8 +3,9 @@ from pygame.locals import *
 
 # Initialize Mixer ------->
 pygame.mixer.init()
+# ------------------------>
 
-# Load Sounds
+# Load Sounds ------------>
 hit = pygame.mixer.Sound("resources/audio/explode.wav")
 enemy = pygame.mixer.Sound("resources/audio/enemy.wav")
 shoot = pygame.mixer.Sound("resources/audio/shoot.wav")
@@ -14,3 +15,4 @@ shoot.set_volume(0.05)
 pygame.mixer.music.load('resources/audio/elevator.mp3')
 pygame.mixer.music.play(-1, 0.0)
 pygame.mixer.music.set_volume(0.25)
+# ------------------------>
